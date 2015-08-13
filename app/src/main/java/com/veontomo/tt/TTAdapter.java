91d09ad4d0ddef24180561a010951ee78120777e
Mario Rossi <veontomo@gmail.com>
2015-08-13 23:38:49 +0200

    feat: create activity that should show all tongue twisters

---
+++ app/src/main/java/com/veontomo/tt/TTAdapter.java
@@ -0,0 +1,31 @@
+package com.veontomo.tt;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Tongue-twister adapter
+ * TODO: implement this class methods
+ */
+public class TTAdapter extends BaseAdapter {
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
---
+++ app/src/main/java/com/veontomo/tt/activities/ShowAllTTActivity.java
@@ -0,0 +1,61 @@
+package com.veontomo.tt.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+
+import com.veontomo.tt.R;
+import com.veontomo.tt.TTAdapter;
+
+public class ShowAllTTActivity extends AppCompatActivity {
+    /**
+     * list view that contains tongue-twisters
+     */
+    private ListView mListView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_all_tt);
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        this.mListView = (ListView) findViewById(R.id.listview);
+        this.mListView.setAdapter(new TTAdapter());
+    }
+
+    @Override
+    public void onPause(){
+        this.mListView.setAdapter(null);
+        this.mListView = null;
+        super.onPause();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_show_all_tt, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
